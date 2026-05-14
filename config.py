bd1c18b5f3903beff13a415c64a9e972304bba3d
adithyakini <67313225+adithyakini@users.noreply.github.com>
2026-05-14 11:23:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
-import os
+import streamlit as st
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
 NEWS_SOURCES = {
     "stocks": [
-        "https://feeds.finance.yahoo.com/rss/2.0/headline?s=%5ENSEI&region=US&lang=en-US",
-        "https://www.moneycontrol.com/rss/business.xml"
+        "https://feeds.finance.yahoo.com/rss/2.0/headline?s=%5ENSEI&region=US&lang=en-US"
     ],
 
     "commodities": [
